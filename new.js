791c3337694034c73e88bd748eed59f4b3f28639
AniketRajendraGade <aniketgade04@gmail.com>
2023-09-27 15:49:01 +0530

    firts commit

---
+++ new.js
@@ -0,0 +1,7 @@
+let typed = new Typed('.ani',{
+    strings:['Software developer','Backend Developer','Frount-End Developer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 200,
+    loop:true,
+  })
